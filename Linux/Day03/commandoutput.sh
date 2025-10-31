145e1207932f61017154a440fb2b29a3a7e6163d
manishab30 <bmanisha987234@gmail.com>
2025-10-31 18:36:28 +0530

    added

---
+++ Linux/Day03/commandoutput.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+SERVER_NAME=$(hostname)
+echo "YOu are running this script on ${SERVER_NAME}."
---
+++ Linux/Day03/ifelse.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+MY_SHELL="csh"
+
+if ["$MY_SHELL" = "bash"]
+then
+  echo "You seem to like the bash shell."
+else
+   echo "You dont seem to like the bash shell"
+fi
+   
+  
